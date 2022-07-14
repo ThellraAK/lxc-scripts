80deea8163482bafeb535c7123df9f88d6f77e7e
ThellraAK <github.com@absurdlybored.com>
2022-07-14 01:25:20 -0800

    Update getit.sh

--- syncrelay/getit.sh
+++ syncrelay/getit.sh
@@ -2,5 +2,8 @@ curl -s https://api.github.com/repos/syncthing/relaysrv/releases/latest \
   | awk -F': ' '/strelaysrv-linux-amd64/ && /\.tar.gz/ {gsub(/"/, "", $(NF)); system("curl -LO " $(NF))}'
 tar xf /strelaysrv-linux-amd64-*.tar.gz -C /root
 mv /root/str*/* /root
+adduser -D relay
+mv /root/* /home/relay
+chown -R relay:relay /home/relay
 crontab -l | { cat; echo "/usr/bin/screen -dmS syncthingrelay su relay -c '/home/relay/strelaysrv -pools= -status-srv=:22068 -listen=:22067  -protocol=tcp4 -debug'
 "; } | crontab -
