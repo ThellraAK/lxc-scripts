d24fbdbb46d2f11c3108d2652a08746c96068c82
ThellraAK <github.com@absurdlybored.com>
2022-07-14 02:16:08 -0800

    Update getit.sh

--- syncrelay/getit.sh
+++ syncrelay/getit.sh
@@ -1,6 +1,7 @@
 curl -s https://api.github.com/repos/syncthing/relaysrv/releases/latest \
   | awk -F': ' '/strelaysrv-linux-amd64/ && /\.tar.gz/ {gsub(/"/, "", $(NF)); system("curl -LO " $(NF))}'
 echo "Grabbed latest relay server tarball"
+# Above taken from https://www.jwillikers.com/one-liner-to-fetch-the-latest-github-release
 tar xf /strelaysrv-linux-amd64-*.tar.gz -C /root
 mv /root/str*/* /root
 echo "extrated it to /root and flattened the directory"
