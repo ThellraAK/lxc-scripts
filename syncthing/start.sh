ae7d2e1130149d661bb255aaca67bf45a232b20a
ThellraAK <github.com@absurdlybored.com>
2022-07-14 23:27:24 -0800

    Update start.sh

--- syncthing/start.sh
+++ syncthing/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # $1 VMID
-# $2 Hostname
+# $2 Hostname 
 # $3 Memory in MB
 # $4 static IP
 
@@ -20,16 +20,23 @@ local:vztmpl/alpine-3.16-default_20220622_amd64.tar.xz \
 --start 1  \
 --swap 0  \
 --features nesting=1  \
--mp0 /mount/rust/syncthing,mp=/Syncthing,ro=1 \
+-mp0 /media/syncthing,mp=/Syncthing,ro=1 \
 --rootfs volume=local-lvm:1,mountoptions=noatime
 echo "Created LXC Container with VMID $1 with hostname $2 allowing it to have $3MB of ram, with a static ip of $4"
 pct exec $1 apk add syncthing
-echo "syncthing"
-#pct push $1 ./cert.pem /root/cert.pem
-#pct push $1 ./key.pem /root/key.pem
+echo "Adding Syncthing, copying scripts over, and adding syncthing to autostart, then start/stopping it so config files are generated."
 pct push $1 ./initscript.sh /root/initscript.sh
-pct exec $1 sh /root/initscript.sh
-echo "running script after copying files, sleeping for 5 seconds, then rebooting"
-sleep 5
+pct push $1 ./privscript.sh /root/privscript.sh
+pct exec $1 -- /bin/ash -c "rc-update add syncthing"
+pct exec $1 -- /bin/ash -c "service syncthing start"
+pct exec $1 -- /bin/ash -c "service syncthing stop"
 echo "rebooting VMID $1"
 pct reboot $1
+echo "rebooted VMID $1"
+echo "Running initscript.sh"
+pct exec $1 -- /bin/ash -c "sh /root/initscript.sh"
+echo "running privscript.sh"
+pct exec $1 -- /bin/ash -c "sh /root/privscript.sh"
+echo "rebooting VMID $1"
+pct reboot $1
+echo "rebooted VMID $1"
