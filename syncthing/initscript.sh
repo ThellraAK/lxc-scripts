8b0f4fb828df7e2b1d201eb17c9b96a506538a4b
ThellraAK <github.com@absurdlybored.com>
2022-07-14 23:29:14 -0800

    Update initscript.sh

--- syncthing/initscript.sh
+++ syncthing/initscript.sh
@@ -1,10 +1,5 @@
-adduser -D syncthing
-rc-update add syncthing
-service syncthing start
-service syncthing stop
-mkdir /Syncthing
-chown -R syncthing:syncthing /Syncthing
 ls -la /var/lib/syncthing/.config/syncthing/config.xml
 sed -i 's+path="/var/lib/syncthing/Sync"+path="/Syncthing"+' /var/lib/syncthing/.config/syncthing/config.xml
 sed -i 's/^        <address>127.0.0.1:8384<\/address>$/        <address>0.0.0.0:8384<\/address>/' /var/lib/syncthing/.config/syncthing/config.xml
-service syncthing start
+cat /var/lib/syncthing/.config/syncthing/config.xml | grep "<address>"
+cat /var/lib/syncthing/.config/syncthing/config.xml | grep "\/Syncthing"
